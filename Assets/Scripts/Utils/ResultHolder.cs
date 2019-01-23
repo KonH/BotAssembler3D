adb3f1e0b1955b33e684d72de3ad26fd71c81e23
konstantin.hitrih <konstantin.hitrih@gmail.com>
2019-01-23 19:09:47 +0700

    Helper for coroutine results

---
+++ Assets/Scripts/Utils/ResultHolder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BotAssembler.Utils {
+	public class ResultHolder<T> {
+		T    _value;
+		bool _isSet = false;
+
+		public void Set(T value) {
+			_isSet = true;
+			_value = value;
+		}
+
+		public T Get() {
+			if ( !_isSet ) {
+				throw new InvalidOperationException("Trying to get result without set it before");
+			}
+			return _value;
+		}
+	}
+}

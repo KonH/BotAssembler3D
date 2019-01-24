cd4c0f9e6eb85e573b991e3388ad732dd975a613
konstantin.hitrih <konstantin.hitrih@gmail.com>
2019-01-24 19:16:35 +0700

    Change result holder interface

--- Assets/Scripts/Utils/ResultHolder.cs
+++ Assets/Scripts/Utils/ResultHolder.cs
@@ -2,19 +2,21 @@
 
 namespace BotAssembler.Utils {
 	public class ResultHolder<T> {
-		T    _value;
-		bool _isSet = false;
+		public bool IsSet { get; private set; }
 
-		public void Set(T value) {
-			_isSet = true;
-			_value = value;
-		}
-
-		public T Get() {
-			if ( !_isSet ) {
-				throw new InvalidOperationException("Trying to get result without set it before");
+		public T Value {
+			get {
+				if ( !IsSet ) {
+					throw new InvalidOperationException("Trying to get result without set it before");
+				}
+				return _value;
 			}
-			return _value;
+			set {
+				_value = value;
+				IsSet = true;
+			}
 		}
+
+		T _value;
 	}
 }
